1abd0ded454895b480852880513ddc64a127fcbb
D1353L <meghanik400@gmail.com>
2016-08-06 15:06:45 +0200

    View book_description refactored

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find_by_id(params[:id])
     @order_item = current_order.order_items.new
-    @description = 'full_description'
+    @description = @book.full_description
     @max_description_length = 2100
   end
 
